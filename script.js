647f3c8cd1996fa16b979303ea7e73b0a2b550b1
etiry <7997357+etiry@users.noreply.github.com>
2023-04-15 12:04:34 -0400

    Add operate function

--- script.js
+++ script.js
@@ -15,3 +15,20 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
 	return (num2 === 0 ? 'Error' : num1 / num2);
 }
+
+let firstOperand = null;
+let secondOperand = null;
+let operator = null;
+
+function operate(a, b, operator) {
+	switch (operator) {
+		case '+':
+			return add(a, b);
+		case '-':
+			return subtract(a, b);
+		case '*':
+			return multiply(a, b);
+		case '/':
+			return divide(a, b);
+	}
+}
