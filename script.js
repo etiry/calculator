9cd0064589df43d8aaf0ae3d85558dfd39b07a78
etiry <7997357+etiry@users.noreply.github.com>
2023-04-15 11:31:03 -0400

    Add initial files
    Add HTML shell, initial arithmetic functions in
    JS file, and empty CSS file

---
+++ script.js
@@ -0,0 +1,17 @@
+// script.js
+
+function add(num1, num2) {
+	return num1 + num2;
+}
+
+function subtract(num1, num2) {
+	return num1 - num2;
+}
+
+function multiply(num1, num2) {
+	return num1 * num2;
+}
+
+function divide(num1, num2) {
+	return (num2 === 0 ? 'Error' : num1 / num2);
+}
